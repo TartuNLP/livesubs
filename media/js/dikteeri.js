e3a438690c296e0129a8e70b7774b29f08512b86
kspar <kaspar.papli@gmail.com>
2017-10-16 17:34:34 +0300

    test prettifying vol3

--- media/js/dikteeri.js
+++ media/js/dikteeri.js
@@ -17,58 +17,58 @@ function capitaliseFirstLetter(string) {
 }
 
 function prettyfyHyp(text, doCapFirst, doPrependSpace) {
-	if (doCapFirst) {
-		text = capitaliseFirstLetter(text);
-	}
-	tokens = text.split(" ");
-	text = "";
-	if (doPrependSpace) {
-		text = " ";
-	}
-	doCapitalizeNext = false;
-	tokens.map(function(token) {
-		if (text.trim().length > 0) {
-			text = text + " ";
-		}
-		if (doCapitalizeNext) {
-			text = text + capitaliseFirstLetter(token);
-		} else {
-			text = text + token;
-		}
-		if (token == "." ||  /\n$/.test(token)) {
-			doCapitalizeNext = true;
-		} else {
-			doCapitalizeNext = false;
-		}
-	});
-	
-	text = text.replace(/ ([,.!?:;])/g,  "\$1");
-	text = text.replace(/ ?\n ?/g,  "\n");
-	return text;
-}	
+    if (doCapFirst) {
+        text = capitaliseFirstLetter(text);
+    }
+    tokens = text.split(" ");
+    text = "";
+    if (doPrependSpace) {
+        text = " ";
+    }
+    doCapitalizeNext = false;
+    tokens.map(function (token) {
+        if (text.trim().length > 0) {
+            text = text + " ";
+        }
+        if (doCapitalizeNext) {
+            text = text + capitaliseFirstLetter(token);
+        } else {
+            text = text + token;
+        }
+        if (token == "." || /\n$/.test(token)) {
+            doCapitalizeNext = true;
+        } else {
+            doCapitalizeNext = false;
+        }
+    });
+
+    text = text.replace(/ ([,.!?:;])/g, "\$1");
+    text = text.replace(/ ?\n ?/g, "\n");
+    return text;
+}
 
 function updateDisabledState() {
-  var disabled = false;
-  var text = "Dikteerimiseks vajuta nuppu";
-  if (!isMicrophoneInitialized) {
-    disabled = true;
-    text = "Mikrofon initsialiseerimata";
-  } else if (isConnected) {
-    disabled = false;
-    text = "Räägi...";
-  } else if (numWorkersAvailable == 0) {
-    disabled = true;
-    text = "Server ülekoormatud või rivist väljas";
-  }
-  if (disabled) {
-    $("#recbutton").addClass("disabled");
-    $("#helptext").html(text);
-  } else {
-    $("#recbutton").removeClass("disabled");
-    $("#helptext").html(text);
-  }
+    var disabled = false;
+    var text = "Dikteerimiseks vajuta nuppu";
+    if (!isMicrophoneInitialized) {
+        disabled = true;
+        text = "Mikrofon initsialiseerimata";
+    } else if (isConnected) {
+        disabled = false;
+        text = "Räägi...";
+    } else if (numWorkersAvailable == 0) {
+        disabled = true;
+        text = "Server ülekoormatud või rivist väljas";
+    }
+    if (disabled) {
+        $("#recbutton").addClass("disabled");
+        $("#helptext").html(text);
+    } else {
+        $("#recbutton").removeClass("disabled");
+        $("#helptext").html(text);
+    }
 }
-  
+
 function getAverage(array) {
     var values = 0;
     var average;
@@ -84,161 +84,166 @@ function getAverage(array) {
 
 function rafCallback(time) {
 
-  var requestAnimationFrame = window.requestAnimationFrame ||
+    var requestAnimationFrame = window.requestAnimationFrame ||
         window.webkitRequestAnimationFrame ||
         window.mozRequestAnimationFrame ||
         window.oRequestAnimationFrame ||
         window.msRequestAnimationFrame;
-  requestAnimationFrame(rafCallback, this);
-  if (isConnected) {
-    var freqByteData = new Uint8Array(userSpeechAnalyser.frequencyBinCount);
-    userSpeechAnalyser.getByteFrequencyData(freqByteData); 
-    var average = getAverage(freqByteData);
-    $("#recbutton").css({"background-color": "rgba(255, 0, 0, " + Math.log(average)/Math.log(256)  +" )"});
-  } else {
-    $("#recbutton").css({"background-color": "rgba(255, 0, 0, 0.0)"});
-  }
+    requestAnimationFrame(rafCallback, this);
+    if (isConnected) {
+        var freqByteData = new Uint8Array(userSpeechAnalyser.frequencyBinCount);
+        userSpeechAnalyser.getByteFrequencyData(freqByteData);
+        var average = getAverage(freqByteData);
+        $("#recbutton").css({"background-color": "rgba(255, 0, 0, " + Math.log(average) / Math.log(256) + " )"});
+    } else {
+        $("#recbutton").css({"background-color": "rgba(255, 0, 0, 0.0)"});
+    }
 }
 
 var dictate = null;
 
-  
+
 function createDictate() {
-  serverBaseUrl = "bark.phon.ioc.ee:8443/konverentsid/duplex-speech-api";
+    serverBaseUrl = "bark.phon.ioc.ee:8443/konverentsid/duplex-speech-api";
 
-  dictate = new Dictate({
-      server : "wss://" + serverBaseUrl + "/ws/speech",
-      serverStatus : "wss://" + serverBaseUrl + "/ws/status",
-      referenceHandler : "https://" + serverBaseUrl + "/dynamic/reference",
-      recorderWorkerPath : "media/js/libs/dictate.js/lib/recorderWorker.js",
+    dictate = new Dictate({
+        server: "wss://" + serverBaseUrl + "/ws/speech",
+        serverStatus: "wss://" + serverBaseUrl + "/ws/status",
+        referenceHandler: "https://" + serverBaseUrl + "/dynamic/reference",
+        recorderWorkerPath: "media/js/libs/dictate.js/lib/recorderWorker.js",
 
-      onReadyForSpeech : function() {
-        isConnected = true;
-        __message("READY FOR SPEECH");
-        $("#recbutton").addClass("playing");
-        $("#helptext").html("Räägi");
-      },
+        onReadyForSpeech: function () {
+            isConnected = true;
+            __message("READY FOR SPEECH");
+            $("#recbutton").addClass("playing");
+            $("#helptext").html("Räägi");
+        },
 
-      onPartialResults : function(hypos) {
-        hypText = prettyfyHyp(hypos[0].transcript, true, false);
-        console.log('Raw: ' + hypos);
-        console.log('Pre: ' + hypos);
+        onPartialResults: function (hypos) {
+            hypText = prettyfyHyp(hypos[0].transcript, true, false);
+            raw = JSON.stringify(hypos);
+            console.log('Raw: ' + raw);
+            console.log('Pre: ' + hypText);
 
-      },
+        },
 
-      onResults : function(hypos) {
-        hypText = prettyfyHyp(hypos[0].transcript, true, false);
-          console.log('Raw: ' + hypos);
-          console.log('Pre: ' + hypos);
+        onResults: function (hypos) {
+            hypText = prettyfyHyp(hypos[0].transcript, true, false);
+            raw = JSON.stringify(hypos);
+            console.log('Raw: ' + raw);
+            console.log('Pre: ' + hypText);
 
-      },
+        },
 
-      onEndOfSpeech : function() {
-          __message("END OF SPEECH");
-          $("#playbutton").addClass("disabled");
-      },
+        onEndOfSpeech: function () {
+            __message("END OF SPEECH");
+            $("#playbutton").addClass("disabled");
+        },
 
-      onEndOfSession : function() {
-          isConnected = false;
-          __message("END OF SESSION");
-          $("#recbutton").removeClass("playing");
-          updateDisabledState();
-          $("#button-toolbar").removeClass("hidden");
-      },
+        onEndOfSession: function () {
+            isConnected = false;
+            __message("END OF SESSION");
+            $("#recbutton").removeClass("playing");
+            updateDisabledState();
+            $("#button-toolbar").removeClass("hidden");
+        },
 
-      onServerStatus : function(json) {
-          __serverStatus(json.num_workers_available);
-          numWorkersAvailable = json.num_workers_available;
-          updateDisabledState();
-      },
+        onServerStatus: function (json) {
+            __serverStatus(json.num_workers_available);
+            numWorkersAvailable = json.num_workers_available;
+            updateDisabledState();
+        },
 
-      onError : function(code, data) {
-        dictate.cancel();
-        __error(code, data);
-        // TODO: show error in the GUI
-      },
+        onError: function (code, data) {
+            dictate.cancel();
+            __error(code, data);
+            // TODO: show error in the GUI
+        },
 
-      onEvent : function(code, data) {
-        __message(code, data);
-        if (code === 3 /* MSG_INIT_RECORDER */) {
-          isMicrophoneInitialized = true;
-          updateDisabledState();
-        }
-      },
+        onEvent: function (code, data) {
+            __message(code, data);
+            if (code === 3 /* MSG_INIT_RECORDER */) {
+                isMicrophoneInitialized = true;
+                updateDisabledState();
+            }
+        },
 
-      rafCallback : rafCallback,
-      content_id : $("#content_id").html(),
-      user_id : $("#user_id").html()
+        rafCallback: rafCallback,
+        content_id: $("#content_id").html(),
+        user_id: $("#user_id").html()
     });
 }
 
 // Private methods (called from the callbacks)
 function __message(code, data) {
-	//console.log("msg: " + code + ": " + (data || ''));
+    //console.log("msg: " + code + ": " + (data || ''));
 }
 
 function __error(code, data) {
-	console.log("ERR: " + code + ": " + (data || ''))
+    console.log("ERR: " + code + ": " + (data || ''))
 }
 
 function __serverStatus(msg) {
-	serverStatusBar.innerHTML = msg;
+    serverStatusBar.innerHTML = msg;
 }
 
 
 // Public methods (called from the GUI)
 function toggleListening() {
-	if (isConnected) {
-		dictate.stopListening();
-    $("#recbutton").addClass("disabled");
-    $("#helptext").html("Oota..");    
-	} else {
-		dictate.startListening();
-	}
+    if (isConnected) {
+        dictate.stopListening();
+        $("#recbutton").addClass("disabled");
+        $("#helptext").html("Oota..");
+    } else {
+        dictate.startListening();
+    }
 }
 
 function cancel() {
-	dictate.cancel();  
+    dictate.cancel();
 }
 
-function clearTranscription() {	
+function clearTranscription() {
 
 }
 
 function resetText() {
-  clearTranscription();
-  var new_uuid = uuid()
-  $("#content_id").html(new_uuid);
-  dictate.getConfig().content_id = new_uuid;
-  $("#button-toolbar").addClass("hidden");
-  $("#submitButton").addClass("disabled");
+    clearTranscription();
+    var new_uuid = uuid()
+    $("#content_id").html(new_uuid);
+    dictate.getConfig().content_id = new_uuid;
+    $("#button-toolbar").addClass("hidden");
+    $("#submitButton").addClass("disabled");
 }
 
 function uuid() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {var r = Math.random()*16|0,v=c=='x'?r:r&0x3|0x8;return v.toString(16);});
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : r & 0x3 | 0x8;
+        return v.toString(16);
+    });
 }
 
 function bookmarkletReturnResult() {
-  console.log($(window.parent.document.dictateTextField).val());
-  console.log($("#trans").val());
-  $(document.dictateTextField).val($("#trans").val());
+    console.log($(window.parent.document.dictateTextField).val());
+    console.log($("#trans").val());
+    $(document.dictateTextField).val($("#trans").val());
 }
 
 
-$(document).ready(function() {
-  $("#show_once_message").cookieBar({ closeButton : '#show_once_message_close_button' });
-  $("#content_id").html(uuid());
-  user_id = $.cookie('dikteeri_user_uuid')
-  if (!user_id) {
-    user_id = uuid();
-    $.cookie('dikteeri_user_uuid', user_id, { expires: 5*365, path: '/' });
-  }
-  $("#user_id").html(user_id);
-  $("#trans").on('input', function() {
-    $("#submitButton").removeClass("disabled");
-  });  
-  createDictate();
-	dictate.init();
-  
-  
+$(document).ready(function () {
+    $("#show_once_message").cookieBar({closeButton: '#show_once_message_close_button'});
+    $("#content_id").html(uuid());
+    user_id = $.cookie('dikteeri_user_uuid')
+    if (!user_id) {
+        user_id = uuid();
+        $.cookie('dikteeri_user_uuid', user_id, {expires: 5 * 365, path: '/'});
+    }
+    $("#user_id").html(user_id);
+    $("#trans").on('input', function () {
+        $("#submitButton").removeClass("disabled");
+    });
+    createDictate();
+    dictate.init();
+
+
 });
