463028eac0afa8c7f7a71a368c8b11a92414b509
kspar <kaspar.papli@gmail.com>
2017-10-30 02:03:04 +0200

    removed debug logging from autoscroll

--- media/js/dikteeri.js
+++ media/js/dikteeri.js
@@ -66,9 +66,6 @@ function createDictate() {
             var transTextEl = $('#trans-text');
             var transWrap = $('#trans')[0];
             var isScrolledToBottom = transWrap.scrollHeight - transWrap.clientHeight <= transWrap.scrollTop + scrollOffsetThresholdPx;
-            console.log("scrollHeight: " + transWrap.scrollHeight);
-            console.log("clientHeight: " + transWrap.clientHeight);
-            console.log("scrollTop: " + transWrap.scrollTop);
 
             transTextEl.empty();
 
